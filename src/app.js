8210552187ca157d2dcea6b70b28a33eb5a5558a
gcruzbull <153864295+gcruzbull@users.noreply.github.com>
2025-04-24 05:16:43 +0000

    excuse generator finished

--- src/app.js
+++ src/app.js
@@ -10,10 +10,10 @@ window.onload = function() {
   console.log("Hello Rigo from the console!");
 
   //write your code here
-  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-  let action = ['ate', 'peed', 'crushed', 'broke'];
-  let what = ['my homework', 'my phone', 'the car'];
-  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  let who = ['The dog', 'My grandma', 'The mailman', 'My brother', 'My friend'];
+  let action = ['ate', 'peed', 'crushed', 'broke', 'burned', 'threw out', 'reteined'];
+  let what = ['my homework', 'my phone', 'the car', 'my notes', 'my book', 'the keys'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying', 'when I was just leaving the house'];
   
   let whoRandom = who[Math.floor(Math.random() * who.length)]
   let actionRandom = action[Math.floor(Math.random() * action.length)]
